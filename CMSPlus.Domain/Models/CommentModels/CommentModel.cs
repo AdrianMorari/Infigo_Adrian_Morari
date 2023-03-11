bae18d317698c9509b5f5b3c7e5f1745dfa692bc
Adrian Morari <adrian.morari.mih@gmail.com>
2023-03-11 18:13:53 +0200

    Added Comment Model and AutoMapper profile

---
+++ CMSPlus.Domain/Models/CommentModels/CommentModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CMSPlus.Domain.Models.CommentModels;
+
+public class CommentModel
+{
+    public string FullName { get; set; } = null!;
+    public string Comment { get; set; } = null!;
+}
